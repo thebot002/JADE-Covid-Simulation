a4c997d3fe8e3ca5b1a281ed98ac7e5afa439aeb
Arnaud Ruymaekers <arnaudr85@gmail.com>
2023-07-08 12:46:32 +0200

    Sim working in one container managed by a controller

--- src/MainContainer.java
+++ src/MainContainer.java
@@ -24,24 +24,24 @@ public class MainContainer {
 
 
 
-//        // Sub container
-//        String container_name = "Container-1";
-//
-//        try {
-//            Runtime runtime = Runtime.instance();
-//            ProfileImpl pc = new ProfileImpl(false);
-//            pc.setParameter(ProfileImpl.CONTAINER_NAME, container_name);
-//            pc.setParameter(ProfileImpl.MAIN_HOST, "localhost");
-//            AgentContainer ac = runtime.createAgentContainer(pc);
-//            ac.start();
-//
-//            // Creating the controller
-//            AgentController controllerAgent = ac.createNewAgent("Controller", "agents.ControllerAgent", new Object[]{container_name});
-//            controllerAgent.start();
-//
-//        }
-//        catch (Exception e){
-//            e.printStackTrace();
-//        }
+        // Sub container
+        String container_name = "Container-1";
+
+        try {
+            Runtime runtime = Runtime.instance();
+            ProfileImpl pc = new ProfileImpl(false);
+            pc.setParameter(ProfileImpl.CONTAINER_NAME, container_name);
+            pc.setParameter(ProfileImpl.MAIN_HOST, "localhost");
+            AgentContainer ac = runtime.createAgentContainer(pc);
+            ac.start();
+
+            // Creating the controller
+            AgentController controllerAgent = ac.createNewAgent("Controller", "agents.ControllerAgent", new Object[]{container_name});
+            controllerAgent.start();
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
